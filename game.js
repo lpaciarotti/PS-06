9f700315a35c9b448b3f212ce9de435211e6f23b
Lautaro Paciarotti <lautaro.paciarotti@hotmail.com>
2020-11-07 00:05:20 -0300

    PS-06: Variables x and y changed for player var

--- game.js
+++ game.js
@@ -5,8 +5,7 @@ var KEY_ENTER = 13,
     KEY_DOWN = 40,
     canvas = null,
     ctx = null,
-    x = 50,
-    y = 50,
+    player = null, //In replacement of the x and y variables
     lastPress = null,
     dir = 0, //Saves the direction of our rectangle
     pause = true; //If the game is in pause
@@ -29,7 +28,7 @@ function paint(ctx) {
     //Draw square
     ctx.fillStyle = '#0f0';
     //x,y,width,height
-    ctx.fillRect(x, y, 10, 10);
+    ctx.fillRect(player.x, player.y, 10, 10);
     //To know which was the last key press
     ctx.fillText('Last Press: ' + lastPress, 0, 20);
     // Draw pause
@@ -57,29 +56,29 @@ function act(){
         }
         // Move Rect
         if (dir == 0) {
-            y -= 10;
+            player.y -= 10;
         }
         if (dir == 1) {
-            x += 10;
+            player.x += 10;
         }
         if (dir == 2) {
-            y += 10;
+            player.y += 10;
         }
         if (dir == 3) {
-            x -= 10;
+            player.x -= 10;
         }
         // Out Screen
-        if (x > canvas.width) {
-        x = 0;
+        if (palyer.x > canvas.width) {
+        player.x = 0;
         }
-        if (y > canvas.height) {
-        y = 0;
+        if (player.y > canvas.height) {
+        player.y = 0;
         }
-        if (x < 0) {
-        x = canvas.width;
+        if (player.x < 0) {
+        player.x = canvas.width;
         }
-        if (y < 0) {
-        y = canvas.height;
+        if (player.y < 0) {
+        player.y = canvas.height;
         }
     }
     // Pause/Unpause
@@ -105,30 +104,33 @@ function init() {
     //Start game
     run();
     repaint();
+    // Create player
+    player = new Rectangle(40, 40, 10, 10);
+    player.fill(ctx);
 }
 //For init to start when page load in order to avoid errors
 window.addEventListener('load', init, false);
 //To know if our rectangle is in intersection with other
-function Rectangle(x, y, width, height) {
-    this.x = (x == null) ? 0 : x;
-    this.y = (y == null) ? 0 : y;
+function Rectangle(player.x, player.y, width, height) {
+    this.player.x = (player.x == null) ? 0 : player.x;
+    this..player.y = (player.y == null) ? 0 : player.y;
     this.width = (width == null) ? 0 : width;
     this.height = (height == null) ? this.width : height;
     this.intersects = function (rect) {
         if (rect == null) {
             window.console.warn('Missing parameters on function intersects');
         } else {
-            return (this.x < rect.x + rect.width &&
-            this.x + this.width > rect.x &&
-            this.y < rect.y + rect.height &&
-            this.y + this.height > rect.y);
+            return (this.player.x < rect.player.x + rect.width &&
+            this.player.x + this.width > rect.player.x &&
+            this.player.y < rect.player.y + rect.height &&
+            this.player.y + this.height > rect.player.y);
         }
     };
     this.fill = function (ctx) {
         if (ctx == null) {
             window.console.warn('Missing parameters on function fill');
         } else {
-            ctx.fillRect(this.x, this.y, this.width, this.height);
+            ctx.fillRect(this.player.x, this.player.y, this.width, this.height);
         }
     };
 }

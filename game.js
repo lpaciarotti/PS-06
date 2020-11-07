5026f5e924ba0f43dee17d6835970e65c9f1c0b3
Lautaro Paciarotti <lautaro.paciarotti@hotmail.com>
2020-11-06 23:58:40 -0300

    PS-06: Function rectangle added

--- game.js
+++ game.js
@@ -108,4 +108,27 @@ function init() {
 }
 //For init to start when page load in order to avoid errors
 window.addEventListener('load', init, false);
-
+//To know if our rectangle is in intersection with other
+function Rectangle(x, y, width, height) {
+    this.x = (x == null) ? 0 : x;
+    this.y = (y == null) ? 0 : y;
+    this.width = (width == null) ? 0 : width;
+    this.height = (height == null) ? this.width : height;
+    this.intersects = function (rect) {
+        if (rect == null) {
+            window.console.warn('Missing parameters on function intersects');
+        } else {
+            return (this.x < rect.x + rect.width &&
+            this.x + this.width > rect.x &&
+            this.y < rect.y + rect.height &&
+            this.y + this.height > rect.y);
+        }
+    };
+    this.fill = function (ctx) {
+        if (ctx == null) {
+            window.console.warn('Missing parameters on function fill');
+        } else {
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    };
+}
